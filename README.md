a5b97b417d80ecc53389aab4f39cf1ddb62e61ef
Ryan Baker <39451853+Zeolitic@users.noreply.github.com>
2020-03-09 17:16:58 -0400

    Done

---
+++ README.md
@@ -0,0 +1,22 @@
+# COMP3008P2
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.19.
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Installation
+
+Navigate to the cloned repo, type npm install.
+
+## Extra
+
+Shown in installation.jpg is an tool that is helpful for creating new components
+Shown in newcomponent.jpg is a easy way to generate components with the tool
+
+## Info
+
+Components are basically different screens
+The "App" component is the parent components. Any changes made on it will be reflected in the whole app.
+This is useful for things like main bars, things you'd want persisting on any page
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+  constructor() { }
+
+  public interval;
+  public index = -1;
+  public array = ['Car enthusiast', 'Video games', 'Computer science major', 'Buisness minor'];
+
+
+  typeWriter() {
+
+    const element = document.getElementById('typingAnimation');
+    this.startTimer(this.wordChoser(), element);
+
+  }
+
+
+
+  startTimer(word: string, element: HTMLElement) {
+    let counter = 0;
+    this.interval = setInterval(() => {
+      if (counter === word.length) {
+        clearInterval(this.interval);
+        this.pause(word, element);
+      } else {
+        element.innerHTML += word.charAt(counter);
+        counter++;
+      }
+    }, 150);
+  }
+
+  pause(word: string, element: HTMLElement) {
+    let counter = 0;
+    this.interval = setInterval(() => {
+      if (counter === 1) {
+        clearInterval(this.interval);
+        this.erase(word, element);
+      } else {
+        counter++;
+      }
+    }, 1000);
+  }
+
+
+  erase(word: string, element: HTMLElement) {
+    let counter = 0;
+    this.interval = setInterval(() => {
+      if (counter === word.length) {
+        clearInterval(this.interval);
+        this.typeWriter();
+      } else {
+        let temp = element.innerHTML;
+        temp = temp.substring(0, temp.length - 1);
+        element.innerHTML = temp;
+        counter++;
+      }
+    }, 90);
+  }
+
+  wordChoser() {
+
+    this.index++;
+    this.index = this.index % this.array.length;
+
+    const word = this.array[this.index];
+    return word;
+  }
+
+  ngOnInit() {
+    this.typeWriter();
+
+  }
+
+}
