6411ea7276abc26693aa6b97dc939a32619a51ef
ryanabaker <39451853+ryanabaker@users.noreply.github.com>
2023-03-30 19:00:55 -0400

    Update stars.component.ts

--- src/app/3d-components/stars/stars.component.ts
+++ src/app/3d-components/stars/stars.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import * as THREE from "three";
+import { WebGLRendererParameters } from 'three';
 import { Base3DRendererComponent } from '../base-3d-renderer.component';
 
 @Component({
@@ -16,9 +17,9 @@ export class StarsComponent extends Base3DRendererComponent {
     this.scene = new THREE.Scene();
 
     const vertices = [];
-    for ( let i = 0; i < 1500; i ++ ) {
-      const x = THREE.MathUtils.randFloatSpread(10);
-      const y = THREE.MathUtils.randFloatSpread(10);
+    for ( let i = 0; i < 100000; i ++ ) {
+      const x = THREE.MathUtils.randFloatSpread(100);
+      const y = THREE.MathUtils.randFloatSpread(100);
       const z = THREE.MathUtils.randFloatSpread(500);
 
       vertices.push( x, y, z );
@@ -36,6 +37,12 @@ export class StarsComponent extends Base3DRendererComponent {
   private animateStars() {
     this.stars.rotateY(0.0001);
     this.stars.rotateX(0.00005);
+  }
+
+  protected override createRenderer() {
+    this.renderer = new THREE.WebGLRenderer({canvas: this.canvas, preserveDrawingBuffer: true, antialias: true, powerPreference: "low-power", alpha: true} as WebGLRendererParameters);
+    this.renderer.setPixelRatio(devicePixelRatio);
+    this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
   }
 
   protected override startRenderingLoop() {
