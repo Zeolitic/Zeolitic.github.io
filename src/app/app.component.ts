5b9510ded8bc1ee1857a4d8a49a7f74ff6e61405
ryanabaker <39451853+ryanabaker@users.noreply.github.com>
2023-03-30 19:08:47 -0400

    remove star and planet on non desktop devices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { ThemeService } from './theme.service';
 import * as AOS from 'aos';
 
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,6 +18,21 @@ import * as AOS from 'aos';
 })
 export class AppComponent implements OnInit{
   constructor(readonly theme: ThemeService) {}
+
+  private userAgent: string = navigator.userAgent || navigator.vendor;
+
+  isMobileDevice = (): boolean => {
+    const regexs = [/(Android)(.+)(Mobile)/i, /BlackBerry/i, /iPhone|iPod/i, /Opera Mini/i, /IEMobile/i]
+    return regexs.some((b) => this.userAgent.match(b))
+  }
+
+  isTabletDevice = (): boolean => {
+    const regex = /(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/
+    return regex.test(this.userAgent.toLowerCase())
+  }
+
+  isDesktopDevice = (): boolean => !this.isMobileDevice() && !this.isTabletDevice()
+
 
   ngOnInit(){
     AOS.init({
